b4905fe1ea4511405f2288477b2482b3fd20d43d
Fragnaud <dylanfragnaud16@gmail.com>
2025-03-25 09:57:51 +0100

    Fix PDO - forcer TCP au lieu de socket

--- php/config.php
+++ php/config.php
@@ -6,18 +6,24 @@ if (session_status() === PHP_SESSION_NONE) {
 $db = parse_url(getenv("JAWSDB_URL"));
 
 try {
-    $pdo = new PDO(
-        sprintf(
-            "mysql:host=%s;port=%s;dbname=%s;charset=utf8mb4",
-            $db["host"],
-            $db["port"],
-            ltrim($db["path"], "/")
-        ),
-        $db["user"],
-        $db["pass"]
+    $dsn = sprintf(
+        "mysql:host=%s;port=%s;dbname=%s;charset=utf8mb4",
+        $db["host"],
+        $db["port"] ?? 3306,
+        ltrim($db["path"], "/")
     );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
+
+    // On force la connexion en TCP (pas de socket)
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4",
+        PDO::ATTR_PERSISTENT => false
+    ];
+
+    // Crée la connexion
+    $pdo = new PDO($dsn, $db["user"], $db["pass"], $options);
+} catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }
 ?>
