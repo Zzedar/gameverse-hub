b4bce0c0d54c6968550c828e14eb20004c228256
Fragnaud <dylanfragnaud16@gmail.com>
2025-03-27 10:34:06 +0100

    responsive full2

--- mini-jeux/classements.php
+++ mini-jeux/classements.php
@@ -31,6 +31,7 @@ $scores = $stmt->fetchAll();
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Classements</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
--- mini-jeux/mini-jeux.php
+++ mini-jeux/mini-jeux.php
@@ -12,6 +12,7 @@ if (!isset($_SESSION["user"])) {
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mini-Jeux - GameVerse Hub</title>
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/mini-jeux.css">
