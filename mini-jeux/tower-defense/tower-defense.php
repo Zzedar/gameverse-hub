7f1a2b18839779f11a2bb20b846c6d8ed940be75
Fragnaud <dylanfragnaud16@gmail.com>
2025-03-27 10:37:45 +0100

    responsive full3

--- mini-jeux/tower-defense/tower-defense.php
+++ mini-jeux/tower-defense/tower-defense.php
@@ -12,6 +12,7 @@ if (!isset($_SESSION["user"])) {
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tower Defense Arena</title>
     <link rel="stylesheet" href="style.css">
     <link  rel="stylesheet" href="../../css/style.css"
