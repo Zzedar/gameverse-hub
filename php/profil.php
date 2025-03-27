2838b23cb433eda4c304801b6e38a87c45e96156
Fragnaud <dylanfragnaud16@gmail.com>
2025-03-27 10:25:51 +0100

    responsive profil2

--- php/profil.php
+++ php/profil.php
@@ -14,6 +14,7 @@ $user = $_SESSION["user"];
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profil - GameVerse Hub</title>
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/profil.css"
